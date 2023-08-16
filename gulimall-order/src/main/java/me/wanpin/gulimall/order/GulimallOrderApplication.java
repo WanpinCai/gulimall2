fe7184e9cfce98fabab15c8a187493e08b8b07bd
Swan-Cai <96821426+Swan-Cai@users.noreply.github.com>
2023-08-16 21:32:02 +0100

    项目初始结构创建

---
+++ gulimall-order/src/main/java/me/wanpin/gulimall/order/GulimallOrderApplication.java
@@ -0,0 +1,13 @@
+package me.wanpin.gulimall.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallOrderApplication.class, args);
+    }
+
+}
