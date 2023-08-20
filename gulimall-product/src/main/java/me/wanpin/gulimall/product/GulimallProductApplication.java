a959ac36c906f933897010f7131f1c97385a6477
Swan-Cai <96821426+Swan-Cai@users.noreply.github.com>
2023-08-20 08:25:06 +0100

    product test fail

--- gulimall-product/src/main/java/me/wanpin/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/me/wanpin/gulimall/product/GulimallProductApplication.java
@@ -1,13 +1,22 @@
 package me.wanpin.gulimall.product;
 
-import org.springframework.boot.SpringApplication;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
 
+/**
+ * 1,整合MyBatis-Plus
+ *  1)依赖导入
+ *  2）配置
+ *    1，配置数据源：导入数据库驱动
+ *
+ */
+
+@MapperScan("me.wanpin.gulimall.product.dao")
 @SpringBootApplication
 public class GulimallProductApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(GulimallProductApplication.class, args);
+            SpringApplication.run(GulimallProductApplication.class, args);
+        }
     }
 
-}
